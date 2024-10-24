544947a652297b4c51243fc720ad65c826566681
HenriqueMartinelliPinheiro <henriquepinheiro18@gmail.com>
2024-10-24 20:16:37 -0300

    atualizacao forma de acesso variaveis de ambiente

--- src/services/govBrAuthService.ts
+++ src/services/govBrAuthService.ts
@@ -1,12 +1,13 @@
 const govBrService = {
 	getGovBrAuthUrl: (): string => {
-		const baseUrl = `${import.meta.env.REACT_APP_GOVBR_URL_PROVIDER}` || '';
-		const clientId = `${import.meta.env.REACT_APP_GOVBR_CLIENT_ID}` || '';
-		const redirectUri = `${import.meta.env.REACT_APP_GOVBR_REDIRECT_URI}` || '';
-		const scopes = `${import.meta.env.REACT_APP_GOVBR_SCOPES}` || '';
+		const baseUrl = import.meta.env.VITE_GOVBR_URL_PROVIDER || '';
+		const clientId = import.meta.env.VITE_GOVBR_CLIENT_ID || '';
+		const redirectUri = import.meta.env.VITE_GOVBR_REDIRECT_URI || '';
+		const scopes = import.meta.env.VITE_GOVBR_SCOPES || '';
 
 		if (!baseUrl || !clientId || !redirectUri || !scopes) {
 			console.error('Variáveis de ambiente não estão definidas corretamente.');
+			return '';
 		}
 
 		return `${baseUrl}/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(
