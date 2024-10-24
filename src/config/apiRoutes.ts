0eee23a5005d7a6d47b0cbfa927b6c26d9ed862e
HenriqueMartinelliPinheiro <henriquepinheiro18@gmail.com>
2024-10-23 21:25:54 -0300

    correcao cors servidor cti

--- src/config/apiRoutes.ts
+++ src/config/apiRoutes.ts
@@ -1,4 +1,5 @@
-const apiBaseUrl = 'http://localhost:4000';
+// const apiBaseUrl = 'http://localhost:4000';
+const apiBaseUrl = 'http://200.135.55.14:4000';
 
 export const apiRoutes = {
 	createAttendance: `${apiBaseUrl}/createAttendance`,
